762380501fdecc3f7368c951ab91291e65cb6d1f
zentus <zentus@cimantra.com>
2018-11-05 00:51:46 +0100

    Add Standard linting
    + Some refactoring

--- app.js
+++ app.js
@@ -5,71 +5,69 @@ const { log, error } = console
 
 // Flags
 // -d, dead
-// Outputs nothing, but stderr if exit code !== 0
+// Outputs nothing but stderr if exit code !== 0
 
 // -q, quiet
 // Outputs only defined success messages, and stderr if exit code !== 0
 
 // Get command line input
 const getInput = process => {
-	const { _: arguments, ...flags } = minimist(process.argv.slice(2))
-	return { arguments, flags }
+  const { _: args, ...flags } = minimist(process.argv.slice(2))
+  return { args, flags }
 }
 
 // Handle shell command
 const handle = (action, options) => shell(action.command)
-	.then(result => {
+  .then(result => {
+    const { stdout, stderr, code } = result
+    const { flags } = getInput(process)
 
-		const {stdout, stderr, code} = result
-		const { flags } = getInput(process)
+    if (code !== 0 && stderr) {
+      if (!flags.d && action.error) log(action.error)
+      throw new Error(stderr)
+    }
 
-		if (code !== 0 && stderr) {
-			if (!flags.d && action.error) log(action.error)
-			throw new Error(stderr)
-		}
+    if (code === 0 && stderr) {
+      if (!flags.d && !flags.q) log(stderr)
+      if (!flags.d && action.warn) log(action.warn)
+      return result
+    }
 
-		if (code === 0 && stderr) {
-			if (!flags.d && !flags.q) log(stderr)
-			if (!flags.d && action.warn) log(action.warn)
-			return result
-		}
+    if (code !== 0) {
+      throw new Error(`Command '${action.command}' returned exit code: ${code}.`)
+    }
 
-		if (code !== 0) {
-			throw `Command '${action.command}' returned exit code: ${code}.`
-		}
+    if (!flags.d && !flags.q && stdout) log(stdout)
+    if (!flags.d && action.success) log(action.success)
 
-		if (!flags.d && !flags.q && stdout) log(stdout)
-		if (!flags.d && action.success) log(action.success)
-
-		return result
-	})
-
+    return result
+  })
 
 // Creates an action queue using program object
 const createQueue = program => {
-	return program.actions
-		.filter(action => action.command)
-		.map(action => {
-			return () => handle(action, program.options)
-		})
+  return program.actions
+    .filter(action => action.command)
+    .map(action => {
+      return () => handle(action, program.options)
+    })
 }
 
 // Get command line arguments and flags
 // Use programCreator to create program object
 // Create and start the action queue
 const run = programCreator => {
-	const input = getInput(process)
-	const program = programCreator(input)
-	const queue = createQueue(program)
+  const input = getInput(process)
+  const program = programCreator(input.args, input.flags)
+  const queue = createQueue(program)
 
-	waterfall(queue)
-		.then(result => {
-			// const lastCommand = {
-			// 	action: program.actions.find(action => action.command === cmd),
-			// 	result
-			// }
-		})
-		.catch(err => error(err))
+  waterfall(queue)
+    .then(result => {
+      // const lastCommand = {
+      //   action: program.actions.find(action => action.command === cmd),
+      //   result
+      // }
+    })
+    .catch(err => error(err))
 }
 
 module.exports = { getInput, run }
--- example.js
+++ example.js
@@ -6,44 +6,42 @@ const path = require('path')
 const has = fs.existsSync
 
 const { run } = require('./app')
-const { log } = console
 const chalk = require('chalk')
 const { green, bold } = chalk
 
-const program = ({ arguments, flags }) => {
-	const message = arguments[0]
-	const coolFile = path.join(cwd, 'cool.file')
-	const needCoolFile = !has(coolFile)
-	const needIce = Boolean(flags.ice) || Boolean(flags.i)
-	const iceFile = path.join(cwd, 'ice')
-	const hasIce = has(iceFile)
-	const needUncoolFile = hasIce && platform === 'win32'
+const program = (args, flags) => {
+  const message = args[0]
+  const coolFile = path.join(cwd, 'cool.file')
+  const needCoolFile = !has(coolFile)
+  const needIce = Boolean(flags.ice) || Boolean(flags.i)
+  const iceFile = path.join(cwd, 'ice')
+  const hasIce = has(iceFile)
+  const needUncoolFile = hasIce && platform === 'win32'
 
-	const giveIce = {
-		command: 'touch ice && echo "cool runnings ❄️" >> ice',
-		success: bold.cyan('Your ice: ❄️❄️❄️')
-	}
+  const giveIce = {
+    command: 'touch ice && echo "cool runnings ❄️" >> ice',
+    success: bold.cyan('Your ice: ❄️❄️❄️')
+  }
 
-	// Create an object like
-	return {
-		actions: [{
-				command: needCoolFile && `echo "So you need a cool file?"`
-			}, {
-				command: needCoolFile && 'touch cool.file',
-				success: green('Created a cool file')
-			}, {
-				command: has(coolFile),
-				success: green('\nNice file')
-			}, {
-				command: has(coolFile) && message && `echo ${message} >> cool.file`,
-				success: green(`Wrote your message to the cool file`)
-			},
-			needIce && giveIce,
-			{
-				command: needUncoolFile && 'touch uncool-file.docx'
-		}].filter(Boolean),
-		options: { hmm: '🤔' }
-	}
+  return {
+    actions: [{
+      command: needCoolFile && `echo "So you need a cool file?"`
+    }, {
+      command: needCoolFile && 'touch cool.file',
+      success: green('Created a cool file')
+    }, {
+      command: has(coolFile),
+      success: green('\nNice file')
+    }, {
+      command: has(coolFile) && message && `echo ${message} >> cool.file`,
+      success: green(`Wrote your message to the cool file`)
+    },
+    needIce && giveIce,
+    {
+      command: needUncoolFile && 'touch uncool-file.docx'
+    }].filter(Boolean),
+    options: { hmm: '🤔' }
+  }
 }
 
 run(program)
