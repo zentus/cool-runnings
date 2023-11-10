93ef1464293faef7c6d9acfa190e823e5d876ce6
zentus <zentus@cimantra.com>
2023-11-10 17:42:50 +0100

    feat: Async/awaitify

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const shell = require('shell-exec').default
 const minimist = require('minimist')
-const waterfall = require('p-waterfall')
 const omit = require('object.omit')
-const { log, error, warn } = console
+const waterfall = require('p-waterfall')
+let shell = require('shell-exec')
+shell = shell || shell.default
 
 const getInput = process => {
   const { _: args, ...flags } = minimist(process.argv.slice(2))
   return { args, flags }
 }
 
-const getPreRunPromise = preRun => new Promise(resolve => {
-  resolve(preRun)
-})
-
-const handleAction = (action, options = {}, preRunPromise, ignoreAction, history) => preRunPromise.then(preRun => {
-  const { flags } = getInput(process)
-
+const overrideFlags = (flags, options) => {
   if (options.quiet) {
     flags.q = true
   }
@@ -31,111 +25,128 @@ const handleAction = (action, options = {}, preRunPromise, ignoreAction, history
     flags.q = false
   }
 
-  if (preRun && !flags.d) {
-    log(preRun)
+  return flags
+}
+
+const handleError = async (action, result, flags) => {
+  const { code, stderr, stdout } = result
+
+  if (typeof action.error === 'function') {
+    action.error = await action.error(result)
+  }
+
+  if (typeof action.warn === 'function') {
+    action.warn = await action.warn(result)
+  }
+
+  if (typeof action.success === 'function') {
+    action.success = await action.success(result)
+  }
+
+  if (code !== 0 && stderr) {
+    if (action.error && !flags.d) console.error(action.error)
+    if (stdout && !flags.q && !flags.d) console.error(stdout)
+    if (action.onError) await action.onError(result)
+    throw new Error(stderr)
+  }
+
+  if (code !== 0 && !stderr) {
+    if (action.error && !flags.d) console.log(action.error)
+    if (stdout && !flags.q && !flags.d) console.error(stdout)
+    if (action.onError) await action.onError(result)
+    const identifier = action.name !== result.index
+      ? `"${action.name}"`
+      : `Command "${action.command}"`
+    throw new Error(`${identifier} exited with code ${code}`)
+  }
+
+  if (code === 0 && stderr) {
+    if (!flags.d && !flags.q) console.warn(stderr)
+    if (action.warn && !flags.d) console.warn(action.warn)
+    if (action.onWarn && !flags.d) await action.onWarn(result)
+  }
+
+  if (stdout && !flags.d && !flags.q) console.log(stdout)
+  if (action.success && !flags.d) console.log(action.success)
+  if (action.onSuccess && !flags.d) await action.onSuccess(result)
+}
+
+const handleIgnoredAction = async (action, flags, history) => {
+  if (flags.v) console.log(`Ignored action ${action.name}`)
+
+  const ignoredResult = {
+    code: null,
+    history,
+    ignored: true,
+    name: action.name,
+    stderr: '',
+    stdout: ''
+  }
+
+  if (typeof action.ignored === 'function') {
+    action.ignored = await action.ignored(ignoredResult)
+  }
+
+  if (action.ignored && !flags.q && !flags.d) console.log(action.ignored)
+  if (action.onIgnored && !flags.q && !flags.d) await action.onIgnored(ignoredResult)
+
+  return new Promise(resolve => resolve(ignoredResult))
+}
+
+const handleAction = async (action, options = {}, ignoreAction, history) => {
+  const input = getInput(process)
+  const flags = overrideFlags(input.flags, options)
+
+  if (ignoreAction) {
+    return handleIgnoredAction(action, flags, history)
+  }
+
+  if (action.preRun && !flags.d) {
+    console.log(action.preRun)
   }
 
   if (action.onPreRun && typeof action.onPreRun === 'function') {
-    action.onPreRun(action)
+    await action.onPreRun(action)
   }
 
-  if (ignoreAction) {
-    if (flags.v) log(`Ignored action ${action.name}`)
+  const rawResult = await shell(action.command)
 
-    const ignoredResult = {
-      code: null,
-      stdout: '',
-      stderr: '',
-      ignored: true,
-      name: action.name,
-      history
-    }
+  if (flags.v) console.log(`Started action: ${action.name}`)
 
-    if (typeof action.ignored === 'function') {
-      action.ignored = action.ignored(ignoredResult)
-    }
-
-    if (action.ignored && !flags.q && !flags.d) log(action.ignored)
-    if (action.onIgnored && !flags.q && !flags.d) action.onIgnored(ignoredResult)
-
-    return new Promise(resolve => resolve(ignoredResult))
+  const result = {
+    ...rawResult,
+    history,
+    ignored: false,
+    index: action.index,
+    name: action.name
   }
 
-  return shell(action.command)
-    .then(rawResult => {
-      if (flags.v) log(`Started action: ${action.name}`)
+  await handleError(action, result, flags)
 
-      const result = {
-        ...rawResult,
-        ignored: false,
-        name: action.name,
-        history
-      }
-      const { stdout, stderr, code } = result
+  if (action.onPostRun && typeof action.onPostRun === 'function') {
+    await action.onPostRun(result)
+  }
 
-      if (typeof action.error === 'function') {
-        action.error = action.error(result)
-      }
-
-      if (typeof action.warn === 'function') {
-        action.warn = action.warn(result)
-      }
-
-      if (typeof action.success === 'function') {
-        action.success = action.success(result)
-      }
-
-      if (code !== 0 && stderr) {
-        if (action.error && !flags.d) error(action.error)
-        if (stdout && !flags.q && !flags.d) error(stdout)
-        if (action.onError && !flags.d) action.onError(result)
-        throw new Error(stderr)
-      }
-
-      if (code !== 0 && !stderr) {
-        if (action.onError && !flags.d) action.onError(result)
-        if (action.error && !flags.d) log(action.error)
-        throw new Error(`Command '${action.command}' returned exit code: ${code}.`)
-      }
-
-      if (code === 0 && stderr) {
-        if (!flags.d && !flags.q) warn(stderr)
-        if (action.warn && !flags.d) warn(action.warn)
-        if (action.onWarn && !flags.d) action.onWarn(result)
-      }
-
-      if (stdout && !flags.d && !flags.q) log(stdout)
-      if (action.success && !flags.d) log(action.success)
-      if (action.onSuccess && !flags.d) action.onSuccess(result)
-
-      return result
-    })
-    .then(result => {
-      if (action.onPostRun && typeof action.onPostRun === 'function') {
-        action.onPostRun(result)
-      }
-
-      return result
-    })
-})
+  return result
+}
 
 const createQueue = program => {
   const history = []
 
   return program.actions
     .map((actionFunc, index) => {
-      return lastExecuted => {
+      return async lastExecuted => {
         if (lastExecuted) {
           history.push(omit(lastExecuted, 'history'))
         }
-        const actionRaw = actionFunc(lastExecuted)
-        const action = {
-          ...actionRaw,
-          name: actionRaw.name || index
+        const action = await actionFunc(lastExecuted, index)
+        const indexedAction = {
+          ...action,
+          index,
+          name: action.name || index
         }
-        const preRunPromise = getPreRunPromise(action.preRun)
         const ignoreAction = !action.command
-        return handleAction(action, program.options, preRunPromise, ignoreAction, history)
+        return handleAction(indexedAction, program.options, ignoreAction, history)
       }
     })
 }
@@ -159,15 +170,12 @@ const getProgram = (programCreator, input) => {
 
   return program
 }
-const run = programCreator => {
+const run = async programCreator => {
   const input = getInput(process)
   const program = getProgram(programCreator, input)
   const queue = createQueue(program)
 
-  return waterfall(queue).catch(err => {
-    error(err)
-    return err
-  })
+  return waterfall(queue)
 }
 
 module.exports = { getInput, run }
